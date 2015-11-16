677285bf964d36d3e94e8edb61354326d2404360
Sebastian Wilgosz <sebastian.wilgosz@gmail.com>
2015-11-17 00:14:30 +0100

    increased response time

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,6 @@ class Comment < ActiveRecord::Base
   belongs_to :article
 
   def rate
-    100.times { |i| ((4399999769.32*(i)*article.comments.count)/434992499.23)/(i*100.0) }
+    (1..1000000).to_a.reduce(0) {|r,i| (r+i+i)/i}
   end
 end
